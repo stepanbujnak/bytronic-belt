f879ab30d4337c24df711117f95cfd98ba8a053f
Stepan Bujnak <stepan.bujnak@gmail.com>
2012-03-12 03:15:17 +0000

    version 1.0

---
+++ timer.c
@@ -0,0 +1,51 @@
+#include "timer.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#pragma interrupt_handler timer_interrupt
+
+static unsigned char timer_initialized = 0;
+static unsigned int miliseconds = 0;
+
+void timer_init(void) {
+  if (!timer_initialized) {
+    asm("sei");
+    TSCR |= TEN;
+    TMSK2 = TOI | TIMER_PRESCALE;
+    TFLG2 |= TOF;
+
+    TOF_INTERRUPT = (unsigned short)&timer_interrupt;
+
+    asm("cli");
+    timer_initialized = 1;
+  }
+}
+
+void timer_set(struct timer *t, unsigned int time) {
+  t->expires_in = miliseconds + time;
+  t->new = 1;
+}
+
+int timer_expired(struct timer *t) {
+  if (!t->new) {
+    return 0;
+  } else {
+    if (t->expires_in < miliseconds) {
+      t->new = 0;
+	  
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+}
+
+void timer_interrupt(void) {
+  miliseconds += 33;
+  
+  if (miliseconds > 60000) {
+  	 miliseconds = 0;
+  }
+  
+  TFLG2 |= TOF;
+}
---
+++ timer.h
@@ -0,0 +1,39 @@
+#ifndef _TIMER_H
+#define _TIMER_H
+
+#define IOREGS_BASE 0x0000
+
+#define _IO8(offset) (*(volatile unsigned char *) (IOREGS_BASE + offset))
+#define _IO16(offset) (*(volatile unsigned short *) (IOREGS_BASE + offset))
+
+#define TIOS    _IO8(0x40)
+#define TCNT    _IO16(0x44)
+#define TSCR    _IO8(0x46)
+#define TMSK2   _IO8(0x4D)
+#define TFLG1   _IO8(0x4E)
+#define TFLG2   _IO8(0x4F)
+
+#define TOI     0x80
+#define TEN     0x80
+#define TOF     0x80
+
+#define TIMER_PRESCALE 3
+#define TOF_INTERRUPT (*(volatile unsigned short *)0x3E5E)
+
+#define TIMER(var) static struct timer var = {0}
+
+struct timer {
+  unsigned int expires_in;
+  unsigned char new;
+};
+
+void timer_init(void);
+void timer_set(struct timer *, unsigned int);
+void timer_interrupt(void);
+void timer_add(struct timer *);
+void timer_remove(struct timer *);
+int timer_running(struct timer *);
+int timer_expired(struct timer *);
+
+
+#endif /* _TIMER_H */
